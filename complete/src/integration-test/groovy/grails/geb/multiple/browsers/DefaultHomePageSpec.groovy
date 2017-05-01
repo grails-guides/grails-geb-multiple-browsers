2264359a63341c71f4500817829cead26e7bc7c5
sdelamo <sergio.delamo@softamo.com>
2017-05-01 10:54:55 +0200

    codenarc

--- complete/src/integration-test/groovy/grails/geb/multiple/browsers/DefaultHomePageSpec.groovy
+++ complete/src/integration-test/groovy/grails/geb/multiple/browsers/DefaultHomePageSpec.groovy
@@ -3,10 +3,11 @@ package grails.geb.multiple.browsers
 import geb.spock.GebSpec
 import grails.test.mixin.integration.Integration
 
+@SuppressWarnings('MethodName')
 @Integration
 class DefaultHomePageSpec extends GebSpec {
 
-    def "verifies there is _<h1>_ header with the text _Welcome to Grails when we visit the home page."() {
+    def 'verifies there is _<h1>_ header with the text _Welcome to Grails when we visit the home page.'() {
         when:
         go '/'
 
